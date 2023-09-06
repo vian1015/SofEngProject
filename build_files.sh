0327bcf36bd5e66521d818d6b4c9e64288d53e96
vian1015 <jovian.marvin@ui.ac.id>
2023-09-06 20:27:12 +0700

    sal

--- build_files.sh
+++ build_files.sh
@@ -2,4 +2,3 @@ pip install -r requirements.txt
 python3.9 manage.py collectstatic
 python3.9 manage.py migrate
 python3.9 manage.py loaddata initial_helloworld_data.json
-python3.9 manage.py runserver
