a594c6473640e095300f998bfb606b441d2eb9f6
vian1015 <jovian.marvin@ui.ac.id>
2023-09-06 18:29:34 +0700

    le debug

--- SofEngProject/settings.py
+++ SofEngProject/settings.py
@@ -11,11 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
 import os
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3v$&lqun@k&k@r3xy3z!^#mx1o#gwcb+ts%eet-j^&jkj%h_qg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
