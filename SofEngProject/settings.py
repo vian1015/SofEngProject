8b4ca32ccb37a1c6b738a36cbe3382b6b454a1c2
vian1015 <jovian.marvin@ui.ac.id>
2023-09-06 20:34:42 +0700

    aw

--- SofEngProject/settings.py
+++ SofEngProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3v$&lqun@k&k@r3xy3z!^#mx1o#gwcb+ts%eet-j^&jkj%h_qg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
