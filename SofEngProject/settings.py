c6f5e98287e08a77d3e019b62013f63217392542
vian1015 <jovian.marvin@ui.ac.id>
2023-09-06 23:26:47 +0700

    try env

--- SofEngProject/settings.py
+++ SofEngProject/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'SofEngProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'vcfqkhdw',
-        'USER': 'vcfqkhdw',
-        'PASSWORD': 'c9D3a6-dd-qjmWGCvNgW86zqpCGav-ka',
-        'HOST': 'topsy.db.elephantsql.com',
-        'PORT': '5432'
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_USER_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
